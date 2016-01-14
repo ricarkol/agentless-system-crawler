d22d354cd2e72571328713212a4e083cd91085d2
Peter Meckiffe <pmeckif1@uk.ibm.com>
2016-01-14 02:14:17 +0000

    added fixes for crawler deployment

--- deployment/deploy-va-metrics-crawler.sh
+++ deployment/deploy-va-metrics-crawler.sh
@@ -19,7 +19,7 @@ ENV=$1
 
 SCP="scp -o StrictHostKeyChecking=no"
 SSH="ssh -o StrictHostKeyChecking=no"
-PACKAGE=vacrawler_0.8-va-crawler_amd64.deb
+
 
 
 if [ "$ENABLE_VA_CRAWLER" = "true" ] 
@@ -39,48 +39,50 @@ then
         # jsut to check what's running there
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo docker ps
 
-	#this is a XXX hack. Forgot to change alchemy-crawler to vacrawler in the pre-removal fpm script
+	    #this is a XXX hack. Forgot to change alchemy-crawler to vacrawler in the pre-removal fpm script
         #$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo cp /etc/init/va-crawler.conf /etc/init/alchemy-crawler.conf
         #$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/service alchemy-crawler "start"
 
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/service va-crawler "stop" || true
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/service va-crawler-host "stop" || true
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/service va-crawler-containers "stop" || true
-        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -r vacrawler || true
-        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg --purge vacrawler || true
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -r va-crawler|| true
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -r va-crawler-host || true
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -r va-crawler-container || true
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg --purge va-crawler || true
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /bin/rm -rf /opt/cloudsight/va-crawler
-
+#        VACRAWLER=va-crawler
         echo "Starting va-crawler on host $host"
-	config_file=${VACRAWLER}.sh
-	echo "env SPACE_ID=$VA_CRAWLER_SPACE_ID" >>$config_file
-	echo "env CLOUDSIGHT_CRAWL_EMIT_URL=$VA_CRAWLER_EMIT_URL" >>$config_file
+        config_file=crawler-config.sh
+        echo "env SPACE_ID=$VA_CRAWLER_SPACE_ID" >$config_file
+        echo "env CLOUDSIGHT_CRAWL_EMIT_URL=$VA_CRAWLER_EMIT_URL" >>$config_file
 
-	#upstart config file
-	upstart_config_file_containers=/etc/init/va-crawler-containers.conf
-	upstart_config_file_host=/etc/init/va-crawler-host.conf
+        #upstart config file
+        upstart_config_file_containers=/etc/init/va-crawler-containers.conf
+        upstart_config_file_host=/etc/init/va-crawler-host.conf
 
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mkdir -p $cloudsight_scripts_dir/config
-	$SCP ../../packaging/created_packages/crawler/$PACKAGE ${SSH_USER}@$host:$PACKAGE
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/apt-get update --fix-missing
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/apt-get install -y --fix-missing  make python2.7 python-pip gcc python-dev rpm uuid
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/apt-get -f install
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mkdir -p $cloudsight_scripts_dir/config
+        $SCP ../packaging/created_packages/crawler/$VACRAWLER_DEB_FILE ${SSH_USER}@$host:$VACRAWLER_DEB_FILE
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/apt-get update --fix-missing
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/apt-get install -y --fix-missing  make python2.7 python-pip gcc python-dev rpm uuid
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/apt-get -f install
 
-	echo "installed vacrawler dependencies"
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -i $PACKAGE || echo "Could not install va-crawler package"
-	echo "installed package"
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -l
-	# We copy the config file just for auditing purposes
-	$SCP $config_file ${SSH_USER}@$host:$config_file
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mv $config_file $cloudsight_scripts_dir/config/$config_file
+        echo "installed vacrawler dependencies"
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -i $VACRAWLER_DEB_FILE || echo "Could not install va-crawler package"
+        echo "installed package"
+        # $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/dpkg -l
+        # We copy the config file just for auditing purposes
+        $SCP $config_file ${SSH_USER}@$host:$config_file
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mv $config_file $cloudsight_scripts_dir/config/$config_file
 
-	# XXX consider using the $config_file instead of changing the upstart config file
-	$SSH ${SSH_USER}@$host HOST=$host "/usr/bin/sudo cat ${upstart_config_file_containers} $cloudsight_scripts_dir/config/$config_file > /tmp/va-crawler.conf"
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mv /tmp/va-crawler.conf ${upstart_config_file_containers} || true
-	$SSH ${SSH_USER}@$host HOST=$host "/usr/bin/sudo cat ${upstart_config_file_host} $cloudsight_scripts_dir/config/$config_file > /tmp/va-crawler.conf"
-	$SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mv /tmp/va-crawler.conf ${upstart_config_file_host} || true
+        # XXX consider using the $config_file instead of changing the upstart config file
+        $SSH ${SSH_USER}@$host HOST=$host "/usr/bin/sudo cat ${upstart_config_file_containers} $cloudsight_scripts_dir/config/$config_file > /tmp/va-crawler.conf"
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mv /tmp/va-crawler.conf ${upstart_config_file_containers} || true
+        $SSH ${SSH_USER}@$host HOST=$host "/usr/bin/sudo cat ${upstart_config_file_host} $cloudsight_scripts_dir/config/$config_file > /tmp/va-crawler.conf"
+        $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo mv /tmp/va-crawler.conf ${upstart_config_file_host} || true
 
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo pip install --no-index --find-links="/opt/cloudsight/python_packages/" psutil bottle requests simplejson pydoubles netifaces kafka-python docker-py pykafka || true
-	echo "Installed pip packages"
+	    echo "Installed pip packages"
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/service va-crawler-host "start" || true
         $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/bin/service va-crawler-containers "start" || true
 
