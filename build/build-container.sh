ea560a808ebebf5e5b2b499591a95c1fce1e42a9
Peter Meckiffe <pmeckif1@uk.ibm.com>
2016-01-27 15:44:54 +0000

    metrics server must now be a sub-dir of cloudsight during deployment

--- build/build-container.sh
+++ build/build-container.sh
@@ -41,7 +41,7 @@ declare -A IMG_TO_DIR=(
   [$REGISTRY_MONITOR_IMG]="../apps/registry_monitor"
   [$VASTAT_REPORTER_IMG]="../apps/vastat_reporter"
   [$CONFIG_AND_METRICS_CRAWLER_IMG]="../collector/config_and_metrics_crawler"
-  [$METRICS_SERVER_IMG]="../../metrics_server"
+  [$METRICS_SERVER_IMG]="../metrics_server"
 )
 
 
