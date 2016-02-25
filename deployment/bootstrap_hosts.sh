a03694b71e236f7d285c0e536bd7dbcd1c73cd7b
Matt <matt.shaw@uk.ibm.com>
2016-02-25 16:58:48 +0000

    Fix #86 Bootstrap individual hosts rathen than env

--- deployment/bootstrap_hosts.sh
+++ deployment/bootstrap_hosts.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
-
 #
 # Vulnerability Advisor host bootstrapping
 # (c) IBM Research 2015
@@ -8,9 +7,18 @@
 
 if [ $# -eq 1 ]
    then
+   echo "------------------------------------------------"
+   echo "Bootstrapping the designated environment:" $1
    ENV=$1
+   TARGET_HOST=
+elif [ $# -eq 2 ]
+   then
+   echo "------------------------------------------------"
+   echo "Bootstrapping:" $2 "only, in environment:" $1
+   ENV=$1
+   TARGET_HOST=$2
 else
-   echo "Usage: $0 <ENV>"
+   echo "Usage: $0 <ENV> [<TARGET_HOST>]"
    exit 1
 fi
 
@@ -20,14 +28,41 @@ fi
 SCP="scp -o StrictHostKeyChecking=no"
 SSH="ssh -o StrictHostKeyChecking=no"
 
+if [ "$TARGET_HOST" != "" ]
+   then
+   HOSTS_LINE=`grep "HOSTS=(" ../config/hosts.${ENV}`
+   HOSTS_ARRAY=$(echo $HOSTS_LINE | sed -e 's/\$//g' -e 's/HOSTS=(//g' -e 's/)//' -e 's/\s\+/\n/g')
+   arrIN=(${HOSTS_ARRAY// / })
+
+   if ! [[ "${arrIN[*]}" =~ "$TARGET_HOST" ]]
+      then
+         echo "------------------------------------------------"
+         echo "This does not exist in $ENV, please choose one of the following to bootstrap:"
+         echo "${arrIN[@]}"
+      exit 1
+   else
+      echo "The host $TARGET_HOST has the IP ${!TARGET_HOST}"
+   fi
+fi
+
 PARTITION_NUMBER=1
 for host in ${!DOCKER_DEVICES[@]}
    do
    DEVICE=${DOCKER_DEVICES[$host]}
-  
+
+   if [ "$TARGET_HOST" != "" ]
+      then
+      if [ "${!TARGET_HOST}" != "$host" ]
+         then
+         continue
+      fi
+   else
+      echo "TARGET_HOST = $host"
+   fi
+
    echo ""
    echo "================================================"
-   echo "BOOTSTRAPING $host"
+   echo "BOOTSTRAPPING $host"
    echo ""
    echo "-------------DOCKER_DEVICES---------------------"
    echo "Initial block devices in host $host"
@@ -41,7 +76,7 @@ for host in ${!DOCKER_DEVICES[@]}
    $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo /usr/sbin/service docker stop
    
    $SCP utils/create_partition.sh ${SSH_USER}@$host:create_partition.sh
-   $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo ./create_partition.sh $DEVICE $DOCKER_PARTITION_START $DOCKER_PARTITION_END $PARTITION_NUMBER $DOCKER_PARTITION_FSTYPE $DOCKER_PARTITION_MOUNTPOINT 
+   $SSH ${SSH_USER}@$host HOST=$host /usr/bin/sudo ./create_partition.sh $DEVICE $DOCKER_PARTITION_START $DOCKER_PARTITION_END $PARTITION_NUMBER $DOCKER_PARTITION_FSTYPE $DOCKER_PARTITION_MOUNTPOINT
 
    STAT=$?
    if [ $STAT -eq 0 ] 
@@ -65,10 +100,21 @@ for host in ${!DOCKER_DEVICES[@]}
    fi
 done
 
+
 PARTITION_NUMBER=1
 for host in ${!DOCKER_DEVICES_LARGE[@]}
    do
    DEVICE=${DOCKER_DEVICES_LARGE[$host]}
+
+   if [ "$TARGET_HOST" != "" ]
+      then
+      if [ "${!TARGET_HOST}" != "$host" ]
+         then
+         continue
+      fi
+   else
+      echo "TARGET_HOST = $host"
+   fi
   
    echo "-------------DOCKER_DEVICES_LARGE---------------"
    echo "Initial block devices in host $host"
@@ -110,6 +156,16 @@ PARTITION_NUMBER=2
 for host in ${!DATA_DEVICES[@]}
    do
    DEVICE=${DATA_DEVICES[$host]}
+
+   if [ "$TARGET_HOST" != "" ]
+      then
+      if [ "${!TARGET_HOST}" != "$host" ]
+         then
+         continue
+      fi
+   else
+      echo "TARGET_HOST = $host"
+   fi
 
    echo "-------------DATA_DEVICES-----------------------"
    echo "Initial block devices in host $host"
