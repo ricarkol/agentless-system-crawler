ac6363ffa0c3b6d7be5361ef9331c85a3bbe1ab6
Cathy Young <youngcat@uk.ibm.com>
2016-04-07 11:22:41 +0100

    alchemy-va/team#196 Remove redundant src files from compliance annotator

--- apps/compliance_annotator/src/build_docker_container.sh
+++
@@ -1,2 +0,0 @@
-#!/bin.bash
-docker build -t kafka_vulnerability_annotator .
--- apps/compliance_annotator/src/run_docker_scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in `docker ps -a | grep kafka_vulnerability_annotator | cut -d' ' -f1`
-do
-    docker rm -f $i
-done
-
-docker run  --name kafka_vulnerability_annotator -d kafka_vulnerability_annotator --kafka-url kafka-cs.sl.cloud9.ibm.com:9092 --receive-topic config --annotation-topic vulnerabilityscan --notification-topic notification
-
