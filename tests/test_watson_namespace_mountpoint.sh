85275fe43a19335b80307fdbc430849339aa7b03
Sastry Duri <sastry@us.ibm.com>
2016-04-21 07:22:51 -0400

    fix for avoiding unnecessary hierarchy in graphana by replacing . with #

--- tests/test_watson_namespace_mountpoint.sh
+++ tests/test_watson_namespace_mountpoint.sh
@@ -6,27 +6,28 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# the namespace specified on the command line contains /. For example,
-# dev/test. The  container namespace should start with dev.test
+# tests the '.' in mount points are replaced with '#' to avoid
+# unnecessary hierarchy in graphana
 # Returns 1 if success, 0 otherwise
 
-NAME=test_watson_namespace
+NAME=test_watson_namespace_mountpoint
 HOST_NAMESPACE=dev/test
 # Cleanup
+rm -f /tmp/$NAME.*
 rm -rf /var/log/crawler_container_logs/watson_test.service_1.service_v003.*/
 docker rm -f $NAME 2> /dev/null > /dev/null
-rm -f /tmp/$NAME.*
 
 timeout 5 python2.7 ../config_and_metrics_crawler/crawler.py --crawlmode OUTCONTAINER \
-	--features=nofeatures \
+	--features=disk \
     --environment watson \
 	--linkContainerLogFiles --frequency 1 --numprocesses 1 \
 	--url file:///tmp/$NAME --format graphite --namespace ${HOST_NAMESPACE} & #2>/dev/null &
 PID=$!
 
+echo 'uuid' >/tmp/set_component_env.sh
 MSG=`uuid`
 # /var/log/messages is linked by default. Here we test that adding it again won't do any harm.
-docker run -d --name $NAME ubuntu bash -c "\
+docker run -d --name $NAME -v /tmp/set_component_env.sh:/etc/set_component_env.sh ubuntu  bash -c "\
     echo CLOUD_APP_GROUP=\'watson_test\' >>/etc/csf_env.properties; \
     echo CLOUD_APP=\'service_1\' >>/etc/csf_env.properties; \
     echo CLOUD_TENANT=\'public\' >>/etc/csf_env.properties; \
@@ -43,9 +44,10 @@ ID1=`docker ps | grep $NAME | awk '{print $1}'`
 sleep 5
 
 # By now the log should be there
-test_log_fc=`find /var/log/crawler_container_logs/dev.test.watson_test.service_1.service_v003.$ID1/* | grep -c "test..log"`
+count=`grep -c set-component-env#sh /tmp/$NAME.*.0`
 
-if [ $test_log_fc == 2 ];
+# two values for each mounted point
+if [ $count == 2 ];
 then
     echo 1;
 else
@@ -54,3 +56,4 @@ fi
 
 docker rm -f $NAME > /dev/null
 rm -rf /var/log/crawler_container_logs/dev.test.*
+rm -f /tmp/$NAME.*
