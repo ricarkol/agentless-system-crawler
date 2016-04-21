0573059b232a6c9f026c1fa6e1f07cc6fddd70d6
Peter Meckiffe <pmeckif1@uk.ibm.com>
2016-04-21 11:41:43 +0100

    alchemy-va/team#239 added statsd containery

--- build/build-container.sh
+++ build/build-container.sh
@@ -46,6 +46,7 @@ declare -A IMG_TO_DIR=(
   [$IMAGE_RESCANNER_IMG]="../apps/image_rescanner"
   [$MT_LOGSTASH_FORWARDER_IMG]="../mt-logstash-forwarder"
   [$VA_PYTHON_BASE_IMG]="../va_python_base"
+  [$STATSD_IMG]="../statsd"
 )
 
 # Get the kelk-base image if required - only useful for Jenkins builds
