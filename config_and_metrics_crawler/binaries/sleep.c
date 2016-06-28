8d9cd7750ff05598af9b7a786bafdc95a3496cce
Ricardo Koller <ricarkol@gmail.com>
2016-06-28 10:39:04 -0400

    Moved config_and_metrics_crawler to crawler

--- config_and_metrics_crawler/binaries/sleep.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-int main()
-{
-	sleep(60);
-	return 0;
-}
--- config_and_metrics_crawler/stop.sh
+++
@@ -1,5 +0,0 @@
-PID=`awk '{print $1}' crawler.pid | head -n 1`
-# kill the whole group
-kill -9 -$PID
-# just kill the parent
-kill -9 $PID
