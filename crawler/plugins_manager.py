76497cf8b1d3ad477f7a5835dd14af1fa8bf3621
Ricardo Koller <kollerr@us.ibm.com>
2016-09-14 16:48:10 -0400

    step 4
    Signed-off-by: Ricardo Koller <kollerr@us.ibm.com>

--- crawler/plugins_manager.py
+++ crawler/plugins_manager.py
@@ -9,10 +9,11 @@ runtime_env = None
 
 container_crawl_plugins = []
 
+
 def _load_plugins(plugin_places=[misc.execution_path('plugins')],
-                    environment='cloudsight',
-                    category_filter={},
-                    filter_func=lambda *arg: True):
+                  environment='cloudsight',
+                  category_filter={},
+                  filter_func=lambda *arg: True):
     pm = PluginManager(plugin_info_ext='plugin')
 
     # Normalize the paths to the location of this file.
@@ -32,16 +33,16 @@ def reload_env_plugin(plugin_places=[misc.execution_path('plugins')],
                       environment='cloudsight'):
     global runtime_env
     _plugins = _load_plugins(plugin_places, environment,
-                                category_filter={"env": IRuntimeEnvironment},
-                                filter_func=lambda plugin:
-                                    plugin.get_environment_name() == environment)
+                             category_filter={"env": IRuntimeEnvironment},
+                             filter_func=lambda plugin:
+                             plugin.get_environment_name() == environment)
 
     try:
         runtime_env = list(_plugins)[0]
     except TypeError:
         raise RuntimeEnvironmentPluginNotFound('Could not find a valid "%s" '
                                                'environment plugin at %s' %
-                                                (environment, plugin_places))
+                                               (environment, plugin_places))
 
     return runtime_env
 
@@ -51,9 +52,14 @@ def get_runtime_env_plugin():
     return runtime_env
 
 
-def reload_container_crawl_plugins(plugin_places=[misc.execution_path('plugins')]):
+def reload_container_crawl_plugins(
+        plugin_places=[misc.execution_path('plugins')]):
     global container_crawl_plugins
-    container_crawl_plugins = list(_load_plugins(plugin_places, category_filter={"crawler": IContainerCrawler}))
+    container_crawl_plugins = list(
+        _load_plugins(
+            plugin_places,
+            category_filter={
+                "crawler": IContainerCrawler}))
 
 
 def get_container_crawl_plugins():
